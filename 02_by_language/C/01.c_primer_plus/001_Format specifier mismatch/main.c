779d2d3484502c421f867d03c5ec69d519f61d6f
heyeepc <2675458141@qq.com>
2026-03-23 14:13:00 +0800

    更新

---
+++ 02_by_language/C/01.c_primer_plus/001_Format specifier mismatch/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main(){
+
+    char alphabet[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    printf("%c",alphabet);
+
+};
